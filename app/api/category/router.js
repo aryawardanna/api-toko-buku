3f1d047e063f16d29dad82d9dce34b2e698ee0fe
aryawardanna <aryamadridfc@gmail.com>
2022-01-13 20:24:54 +0800

    add category CRUD

--- app/api/category/router.js
+++ app/api/category/router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { auth } = require('../../middlewares/auth');
+const controller = require('./controller');
 
-router.get('/categories', auth, function (req, res) {
-  res.status(200).json({ message: 'Router auth' });
-});
+router.get('/categories', auth, controller.getAllCategories);
+router.post('/categories', auth, controller.createCategories);
+router.put('/categories/:id', auth, controller.updateCategories);
+router.delete('/categories/:id', auth, controller.deleteCategories);
 
 module.exports = router;
