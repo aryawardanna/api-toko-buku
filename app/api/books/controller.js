7d86f88c5da0f06f34e980b3cbe4036ea71a9d81
aryawardanna <aryamadridfc@gmail.com>
2022-01-14 10:45:05 +0800

    add book api

--- app/api/books/controller.js
+++ app/api/books/controller.js
@@ -30,4 +30,107 @@ module.exports = {
       next(error);
     }
   },
+
+  createBooks: async (req, res, next) => {
+    try {
+      let user = req.user.id;
+      const { title, price, category, author, published, stok, image } =
+        req.body;
+
+      const checkCategory = await Category.findOne({
+        where: {
+          id: category,
+          user,
+        },
+      });
+
+      if (!checkCategory) {
+        return res.status(404).json({ message: 'id category not found' });
+      }
+
+      const books = await Book.create({
+        title,
+        price,
+        category,
+        author,
+        published,
+        stok,
+        image,
+        user,
+      });
+      res.status(201).json({
+        message: 'Success create books',
+        books,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  updateBooks: async (req, res, next) => {
+    try {
+      let user = req.user.id;
+      const { id } = req.params;
+      const { title, price, category, author, published, stok, image } =
+        req.body;
+
+      const checkCategory = await Category.findOne({
+        where: {
+          id: category,
+          user,
+        },
+      });
+
+      if (!checkCategory) {
+        return res.status(404).json({ message: 'id category not found' });
+      }
+
+      const checkBook = await Book.findOne({
+        where: { id: id },
+      });
+
+      if (!checkBook) {
+        return res.status(404).json({
+          message: 'id category not found',
+        });
+      }
+
+      const books = await checkBook.update({
+        title,
+        price,
+        category,
+        author,
+        published,
+        stok,
+        image,
+        user,
+      });
+      res.status(200).json({
+        message: 'Success update books',
+        books,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  deleteBooks: async (req, res, next) => {
+    try {
+      const books = await Book.findOne({ where: { id: req.params.id } });
+
+      if (!books) {
+        return res.status(404).json({
+          message: 'id book not found',
+        });
+      }
+      books.destroy();
+
+      res.status(200).json({
+        message: 'Success delete books',
+        data: books,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
 };
--- app/api/books/router.js
+++ app/api/books/router.js
@@ -4,5 +4,8 @@ const { auth } = require('../../middlewares/auth');
 const controller = require('./controller');
 
 router.get('/books', auth, controller.getAllBooks);
+router.post('/books', auth, controller.createBooks);
+router.put('/books/:id', auth, controller.updateBooks);
+router.delete('/books/:id', auth, controller.deleteBooks);
 
 module.exports = router;
